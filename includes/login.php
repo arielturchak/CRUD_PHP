37279592374530c8175198bcf535fddf955345df
[arielturchak] <[arielturchak@hotmail.com]>
2024-01-07 19:22:57 +0100

    tercer commit

--- includes/login.php
+++ includes/login.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -7,12 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
     <link rel="stylesheet" href="../css/estilosLogin.css">
-    <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 </head>
-
+<!--Pregunta
+PDO (PHP Data Objects) en bases de datos es una interfaz de PHP que permite interactuar
+con distintos sistemas de gestión de bases de datos utilizando un conjunto común de métodos, 
+lo que facilita la conexión y manipulación de datos.
+VENTAJAS
+1-Portabilidad del código entre distintos SGBD.
+2-Mayor seguridad mediante el uso de sentencias preparadas.
+3-Mejor rendimiento y más funcionalidades como transacciones y manejo de errores.
+4-Compatibilidad con varios tipos de bases de datos.
+-->
 <body class="contenedor" >
     <div class="login-box">
         <form  action="_functions.php" method="POST">
